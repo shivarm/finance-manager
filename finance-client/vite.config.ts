1ac9f57d46c4ad6063a485df5cdecd2868f0fccc
Shivam Sharma <meshivam81@gmail.com>
2026-06-13 17:22:28 +0530

    feat: configure api proxy url (#6)

--- finance-client/vite.config.ts
+++ finance-client/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://finance-manager-ovts.onrender.com",
         changeOrigin: true,
         secure: false,
       },
